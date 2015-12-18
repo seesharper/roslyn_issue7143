b217a7b696d2db8a812b96831c44aee83f199861
Bernhard Richter <bri@dips.local>
2015-12-18 10:56:51 +0100

    Added build script

--- Foo.cs
+++ Foo.cs
@@ -3,6 +3,9 @@ public class Foo
 {	   	
 	public void Do()
 	{
-		const TypeAttributes TypeAttributes = TypeAttributes.Public;						
+		const TypeAttributes TypeAttributes = TypeAttributes.Public;			
+		
+		// Replace with this line to make it compile. 
+		//const TypeAttributes typeAttributes = TypeAttributes.Public;			
 	}	
 }
